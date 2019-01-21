f44badf7d7f00d15aa6dc6afeb2ede120251e401
Ibrahim Essam <yonohub.team@yonohub.com>
2019-01-21 14:59:06 +0200

    uploading samples

--- kitti_labels.py
+++ kitti_labels.py
@@ -1,5 +1,7 @@
 import numpy as np
 import csv
+import matplotlib.pyplot as plt
+
 class LabelFile:
     ''' Read a label file and return list of labels each represetned as a dict '''
     def __init__(self, labelfile):
@@ -50,7 +52,7 @@ class CalibFile:
 
 
 
-def computeBox3D(label, calib):
+def computeBox3D(label, P):
     ''' this function take CalibFile Object and a Label dict
         and computes the 3D Box 
     '''
@@ -81,10 +83,32 @@ def computeBox3D(label, calib):
     if any(corners_3D[2,:] <0.1):
         return []
     
-    return corners_3D
+    return projectToImage(corners_3D, P)
 
 def projectToImage(pts_3D, P):
     pts_2D = P @ np.vstack((pts_3D,np.ones((1,pts_3D.shape[1]))))
     pts_2D[0,:] = pts_2D[0,:] / pts_2D[2,:]
     pts_2D[1,:] = pts_2D[1,:] / pts_2D[2,:]
     return pts_2D[:2]
+
+
+def computeOrientation3D(label, P):
+    ry = label['ry']
+    R = np.array([[np.cos(ry),  0, np.sin(ry)],
+        [0, 1, 0],
+        [-np.sin(ry), 0, np.cos(ry)]])
+    #orientation in label coordinate system
+    orientation_3D = np.array([[0.0, label['l']],
+                  [0.0, 0.0],
+                  [0.0, 0.0]])
+    #rotate and translate in camera coordinate system, project in image
+    orientation_3D      = R @ orientation_3D
+    orientation_3D[0,:] = orientation_3D[0,:] + label['t'][0]
+    orientation_3D[1,:] = orientation_3D[1,:] + label['t'][1]
+    orientation_3D[2,:] = orientation_3D[2,:] + label['t'][2]
+    if any(orientation_3D[2,:]<0.1):
+        orientation_2D = []
+        return orientation_2D
+    orientation_2D = projectToImage(orientation_3D, P)
+    return orientation_2D
+
